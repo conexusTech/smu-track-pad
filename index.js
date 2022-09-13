93aae329e4a70fb70ffca728d47f536cd30fc732
drexel-conexus <drexel.timogtimog@conexus-tech.com>
2022-09-13 23:58:39 +0800

    fix

--- index.js
+++ index.js
@@ -8,11 +8,8 @@ const wss = new WebSocket.Server({ server:server });
 
 wss.on('connection', function connection(ws) {
   console.log('A new client Connected!');
-  ws.send('Welcome New Client!');
-
   ws.on('message', function incoming(message) {
     console.log('pointer event: %s', message);
-
     wss.clients.forEach(function each(client) {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
         client.send(message);
