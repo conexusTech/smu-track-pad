afb731609ad77e3b6f981fcc89dd34dd3b668200
Drexel9118 <fitzrosh@gmail.com>
2022-09-15 21:27:46 +0800

    Add ws id

--- index.js
+++ index.js
@@ -3,22 +3,26 @@ const app = express()
 const server = require('http').createServer(app);
 const WebSocket = require('ws');
 const path = require('path');
+const nanoid = require('nanoid')
 
 const wss = new WebSocket.Server({ server:server });
 
 wss.on('connection', function connection(ws, req) {
-  console.log(req.url)
-  console.log('A new client Connected!');
+  ws.id = nanoid(10)
+  ws.send(JSON.stringify({connectionId: ws.id}));
   ws.on('message', function incoming(message) {
     // console.log('pointer event: %s', message);
-    console.log(wss.clients)
     wss.clients.forEach(function each(client) {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(message);
+        client.send(JSON.stringify(message));
       }
     });
     
   });
+  ws.on('close', function () {
+    console.log('i was closed')
+    delete ws.id
+  })
 });
 
 app.get('/', (req, res) => {
